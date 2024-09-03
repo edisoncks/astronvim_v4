76e47c0e30ef2a17a1ce230d096314917439b56d
Edison Chong <kurai.no.yozora@gmail.com>
2024-09-03 20:07:10 +0800

    feat: powershell default shell

--- lua/polish.lua
+++ lua/polish.lua
@@ -2,3 +2,10 @@
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
+-- Set PowerShell as the default shell
+-- :help shell-powershell
+vim.cmd [[ let &shell = executable('pwsh') ? 'pwsh' : 'powershell' ]]
+vim.cmd [[ let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;' ]]
+vim.cmd [[ let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode' ]]
+vim.cmd [[ let &shellpipe  = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode' ]]
+vim.cmd [[ set shellquote= shellxquote= ]]
